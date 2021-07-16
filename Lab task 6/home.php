6ae90b05e9549ac10f23bc784532287784f982de
mahirrish1999 <70573837+mahirrish1999@users.noreply.github.com>
2021-07-16 19:21:35 +0600

    Add files via upload

---
+++ Lab task 6/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<fieldset style="width: 800px;margin-left:25%">
+<legend>This is a test</legend>
+<ul style="list-style-type: none;margin:0;padding:0;float:right;width:100%;text-align:right;">
+  <li style="display:inline"><a href="#home">Home</a></li>
+  <li style="display:inline"><a href="login.php">Login</a></li>
+  <li style="display:inline"><a href="registration.php">REGISTRATION</a></li>
+  <hr style="border:0.1px solid;color:#cccccc;">
+</ul>
+<h1><b>Ecomerce site </b></h1>
+<footer style="text-align:center;width:100%;bottom:0;left:0;margin-top:140px;">
+<hr style="border:0.1px solid;color:#cccccc;">
+  <a>This is a test </a></p>
+</footer>
+</fieldset>
+
+</body>
+</html>
